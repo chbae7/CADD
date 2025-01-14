6531c58c94f36e02a1684eccd493e04c99be6b56
chbae7 <ssaybch@naver.com>
2025-01-14 19:53:00 +0900

    Update chbae_CADD.py

--- chbae_CADD.py
+++ chbae_CADD.py
@@ -1,15 +1,4 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import sys, platform
-from prody import *
-from pathlib import Path
-from rdkit import Chem
-from rdkit.Chem import AllChem
-import rdkit, py3Dmol
-from ipywidgets import interact, IntSlider
-import ipywidgets, copy
-from IPython.display import display, Markdown
 
 st.write("rdkit version:", rdkit.__version__)
 st.write("py3Dmol version:", py3Dmol.__version__)
